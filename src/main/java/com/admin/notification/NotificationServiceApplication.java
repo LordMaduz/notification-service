dda9511d2a602bdf4e69af2573396d80cb3fb407
Ruchira Madhushan Rajapaksha <52396694+LordMaduz@users.noreply.github.com>
2024-06-23 19:09:16 +0800

    Update NotificationServiceApplication.java

--- src/main/java/com/admin/notification/NotificationServiceApplication.java
+++ src/main/java/com/admin/notification/NotificationServiceApplication.java
@@ -4,11 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Notification Manager Service", version = "1.0", description = "Notification Manager Service"))
-@EnableFeignClients
+@EnableReactiveMongoRepositories
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
